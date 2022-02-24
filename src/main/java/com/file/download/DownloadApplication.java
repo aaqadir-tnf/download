26266ede8c13e3f3edb2b913aabfae2d09598906
Ansari <Abdulqadir.Ansari@informa.com>
2022-02-24 17:12:13 +0530

    optimise logic to release memory after completeion

--- src/main/java/com/file/download/DownloadApplication.java
+++ src/main/java/com/file/download/DownloadApplication.java
@@ -1,12 +1,7 @@
 package com.file.download;
 
 import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.opencsv.CSVWriter;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -17,12 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.zeroturnaround.zip.ZipUtil;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class DownloadApplication {
@@ -30,17 +27,13 @@ public class DownloadApplication {
 
     //static String destination = "/home/dartsapp/temp/";
     static String destination = "/home/dartsapp/temp/DARTS1456/";
-    //static String destination = "D:\\ToBeDeleted\\";
 
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(DownloadApplication.class, args);
 
-        //try (Reader reader = Files.newBufferedReader(Paths.get("D:\\demo_projects\\download\\src\\main\\resources\\inputcsv.csv"));
-        //try (Reader reader = Files.newBufferedReader(Paths.get("swwf_inputcsv.csv"));
-        //try (Reader reader = Files.newBufferedReader(Paths.get("cwee_inputcsv.csv"));
         try (Reader reader = Files.newBufferedReader(Paths.get("wtss_inputcsv.csv"));
              CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
+            ProcessBuilder processBuilder = new ProcessBuilder();
             for (CSVRecord csvRecord : csvParser) {
                 String name = csvRecord.get("name");
                 String archive_location = csvRecord.get("archive_location");
@@ -50,80 +43,73 @@ public class DownloadApplication {
                 if (Files.isDirectory(Paths.get(archive_location))) {
                     Instant zipStart = Instant.now();
                     logger.info("----Directory Exist, start zipping---");
-
                     String zipFilePath = destination + name + ".zip";
-                    //ZipUtil.pack(new File("D:\\ToBeDeleted\\swwf.cab81.0138_b313822f-0bdd-489b-b4f7-f01d7175f35e"), new File(destination+name+".zip"));
                     ZipUtil.pack(new File(archive_location), new File(zipFilePath));
                     Instant uploadStart = Instant.now();
                     logger.info("----start uploading---");
-                    //uploadObject(new File(zipFilePath));
-                    uploadObjectUsingCLI(new File(zipFilePath));
+                    uploadObjectUsingCLI(new File(zipFilePath), processBuilder);
+                    appendOutputFile(name);
 
-                    Instant uploadEnd = Instant.now();
-                    logger.info("----uploading Done---");
-
-                    Duration ziptimeElapsed = Duration.between(zipStart, uploadStart);
-                    Duration uploadtimeElapsed = Duration.between(uploadStart, uploadEnd);
-
-                    logger.info("Time taken to zip: "+ ziptimeElapsed.toMillis() +" milliseconds");
-                    logger.info("Time taken to upload: "+ uploadtimeElapsed.toMillis() +" milliseconds");
-
+                    //Instant uploadEnd = Instant.now();
+                    logger.info("Time taken to zip: "+ Duration.between(zipStart, uploadStart) +" milliseconds");
+                    //logger.info("Time taken to upload: "+ Duration.between(uploadStart, uploadEnd) +" milliseconds");
                 } else {
                     logger.info("***Not found, Hence ignoring this path****");
                 }
-
             }
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    private static void appendOutputFile(String name) {
+        logger.info("----appendOutputFile start---");
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter("output.csv", true))) {
+            csvWriter.writeNext(new String[]{name});
+            logger.info("----csvWriter end---");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     //upload using AWS CLI, its is comparatively faster
-    private static void uploadObjectUsingCLI(File file) {
-        logger.info("----uploadObjectUsingCLI method called---");
-        Regions clientRegion = Regions.US_EAST_2;
+    private static void uploadObjectUsingCLI(File file, ProcessBuilder processBuilder) {
+        logger.info("----uploadObjectUsingCLI start---");
         String bucketName = "coherent-commons-digital-assets-source";
-        String bucketFullPath = bucketName+"/BRHO/";
+        String bucketFullPath = bucketName+"/BRHO/"; //folder for WTSS
         String nameOfFileToStore = file.getName();
         try {
-            Instant start = Instant.now();
             // Get an object and print its contents.
-            logger.info("---uploading an object with name :: "+nameOfFileToStore + " to bucket :: "+bucketFullPath+"/"+nameOfFileToStore);
-            ProcessBuilder processBuilder = new ProcessBuilder();
-            //String command = "aws s3 cp "+ nameOfFileToStore +"s3://"+bucketName;
-            String command = "aws s3 mv "+ nameOfFileToStore +" s3://"+bucketFullPath;
-            logger.info("---AWS cli:: "+command);
-            processBuilder.command("bash", "-c", command);
-            processBuilder.start();
+            StringBuilder com = new StringBuilder();
+            com.append("aws s3 mv ");
+            com.append(nameOfFileToStore);
+            com.append(" s3://");
+            com.append(bucketFullPath);
 
-            Instant end = Instant.now();
-            Duration timeElapsed = Duration.between(start, end);
-            logger.info("---Time taken uploadObjectUsingCLI: "+ timeElapsed.toMillis() +" milliseconds");
+            logger.info("---AWS cli:: "+com);
+            processBuilder.command("bash", "-c", String.valueOf(com));
+            Process proStart = processBuilder.start();
 
+            stopProcessOnCompletion(proStart);
+
+            logger.info("---uploadObjectUsingCLI completed");
         } catch (SdkClientException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    //upload using AWS SDK, its is comparatively slower AWS CLI
-    private static File uploadObject(File file) {
-        logger.info("----uploadObject method called---");
-        Regions clientRegion = Regions.US_EAST_2;
-        String bucketName = "coherent-commons-digital-assets-source";
-        String nameOfFileToStore = file.getName();
+    private static void stopProcessOnCompletion(Process process) {
+        logger.info("---stopProcessOnCompletion");
         try {
-            AmazonS3 s3client = AmazonS3ClientBuilder
-                    .standard()
-                    .withRegion(clientRegion)
-                    .withCredentials(new DefaultAWSCredentialsProviderChain())
-                    .build();
-            logger.info("----AmazonS3ClientBuilder build---");
-            s3client.putObject(new PutObjectRequest(bucketName, nameOfFileToStore, file)
-                    .withCannedAcl(CannedAccessControlList.BucketOwnerFullControl));
-
-        } catch (SdkClientException e) {
-            logger.info("----SdkClientException---");
+            process.waitFor(1, TimeUnit.MINUTES);
+            logger.info("---waitFor 1 minutes");
+            process.destroy();
+            logger.info("---destroy called");
+            process.waitFor();
+            logger.info("---process destroyed");
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return file;
     }
 }
 
