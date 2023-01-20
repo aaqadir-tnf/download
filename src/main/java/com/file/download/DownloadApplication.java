26c129890fd9f2771b94fdbe57bc2bd73645ecdc
AbdulQadir Ansari <82140712+abdulqadir-ansari@users.noreply.github.com>
2023-01-20 20:27:18 +0530

    read list of items in a folder

--- src/main/java/com/file/download/DownloadApplication.java
+++ src/main/java/com/file/download/DownloadApplication.java
@@ -9,7 +9,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.zeroturnaround.zip.ZipUtil;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -17,8 +16,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.Duration;
-import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
@@ -27,60 +25,43 @@ public class DownloadApplication {
     private static final Logger logger = LogManager.getLogger(DownloadApplication.class);
 
     static String destination = "/home/dartsapp/temp/";
-    //static String destination = "/home/dartsapp/temp/cwee_instance_2/";
-    //static String destination = "/home/dartsapp/temp/cwee_3/";
+    static String inputFile = "dove_issues.csv";
 
 
     public static void main(String[] args) {
         SpringApplication.run(DownloadApplication.class, args);
 
-        try (Reader reader = Files.newBufferedReader(Paths.get("dove_issues_qa.csv"));
-        //try (Reader reader = Files.newBufferedReader(Paths.get("C:\\home\\demo_projects\\download\\src\\main\\resources\\dove_issues_qa.csv"));
+        try (Reader reader = Files.newBufferedReader(Paths.get(inputFile));
              CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
-            ProcessBuilder processBuilder = new ProcessBuilder();
             for (CSVRecord csvRecord : csvParser) {
                 String name = csvRecord.get("name");
-                String archive_location = csvRecord.get("archive_location");
+                String archiveLocation = csvRecord.get("archive_location");
                 logger.info("Name: " + name);
-                logger.info("Archive_location: " + archive_location);
+                logger.info("ArchiveLocation: " + archiveLocation);
 
-                try {
-                    if (Files.isDirectory(Paths.get(archive_location))) {
-                        logger.info("----Directory Exist, GetNames---");
-                        File directoryPath = new File(archive_location);
-                        String[] contents = directoryPath.list();
+                if (Files.isDirectory(Paths.get(archiveLocation))) {
+                    logger.info("----Directory Exist, GetNames---");
+                    File directoryPath = new File(archiveLocation);
+                    String[] contents = directoryPath.list();
 
-                        logger.info("----content found: "+ Arrays.stream(contents).count());
-                        logger.info("----appendOutputFile start---");
-                        try (CSVWriter csvWriter = new CSVWriter(new FileWriter(name + ".csv", false))) {
-                            csvWriter.writeNext(new String[]{name});
-                            csvWriter.writeNext(new String[]{Arrays.toString(contents)});
-                            logger.info("----csvWriter end---");
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    /*Instant zipStart = Instant.now();
-                    logger.info("----Directory Exist, start zipping---");
-                    String zipFilePath = destination + name + ".zip";
-                    ZipUtil.pack(new File(archive_location), new File(zipFilePath));
-                    Instant uploadStart = Instant.now();
-                    logger.info("----start uploading---");
-                    uploadObjectUsingCLI(new File(zipFilePath), processBuilder);
-                    appendOutputFile(name);*/
-
-                        //Instant uploadEnd = Instant.now();
-                        //logger.info("Time taken to zip: "+ Duration.between(zipStart, uploadStart) +" milliseconds");
-                        //logger.info("Time taken to upload: "+ Duration.between(uploadStart, uploadEnd) +" milliseconds");
-                    } else {
-                        logger.info("***Not found, Hence ignoring this path****");
+                    ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(contents));
+                    logger.info("----content found: " + arrayList.size());
+                    logger.info("----appendOutputFile start---");
+                    //all issues output in one file
+                    //try (CSVWriter csvWriter = new CSVWriter(new FileWriter("ListOfArticles" + ".csv", true))) {
+                        //each issues output will be in their individual file
+                      try (CSVWriter csvWriter = new CSVWriter(new FileWriter(name + ".csv", true))) {
+                        csvWriter.writeNext(new String[]{name + ": " + arrayList.size() + " items found\n"});
+                        csvWriter.writeNext(new String[]{Arrays.toString(contents) + "\n"});
+                        logger.info("----csvWriter end---");
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (Exception ex){
-                    ex.getCause();
+                } else {
+                    logger.info("***Not found, Hence ignoring this path****");
                 }
             }
-        } catch (IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
@@ -91,32 +72,6 @@ public class DownloadApplication {
             csvWriter.writeNext(new String[]{name});
             logger.info("----csvWriter end---");
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //upload using AWS CLI, its is comparatively faster
-    private static void uploadObjectUsingCLI(File file, ProcessBuilder processBuilder) {
-        logger.info("----uploadObjectUsingCLI start---");
-        String bucketName = "coherent-commons-digital-assets-source";
-        String bucketFullPath = bucketName+"/CWEE/"; //folder for CWEE
-        String nameOfFileToStore = file.getName();
-        try {
-            // Get an object and print its contents.
-            StringBuilder com = new StringBuilder();
-            com.append("aws s3 mv ");
-            com.append(nameOfFileToStore);
-            com.append(" s3://");
-            com.append(bucketFullPath);
-
-            logger.info("---AWS cli:: "+com);
-            processBuilder.command("bash", "-c", String.valueOf(com));
-            Process proStart = processBuilder.start();
-
-            stopProcessOnCompletion(proStart);
-
-            logger.info("---uploadObjectUsingCLI completed");
-        } catch (SdkClientException | IOException e) {
             e.printStackTrace();
         }
     }
@@ -135,27 +90,6 @@ public class DownloadApplication {
         }
     }
 
-    //upload using AWS SDK, its is comparatively slower than AWS CLI
-    /*private static File uploadObject(File file) {
-        logger.info("----uploadObject method called---");
-        Regions clientRegion = Regions.US_EAST_2;
-        String bucketName = "coherent-commons-digital-assets-source";
-        String nameOfFileToStore = file.getName();
-        try {
-            AmazonS3 s3client = AmazonS3ClientBuilder
-                    .standard()
-                    .withRegion(clientRegion)
-                    .withCredentials(new DefaultAWSCredentialsProviderChain())
-                    .build();
-            logger.info("----AmazonS3ClientBuilder build---");
-            s3client.putObject(new PutObjectRequest(bucketName, nameOfFileToStore, file)
-                    .withCannedAcl(CannedAccessControlList.BucketOwnerFullControl));
 
-        } catch (SdkClientException e) {
-            logger.info("----SdkClientException---");
-            e.printStackTrace();
-        }
-        return file;
-    }*/
 }
 
